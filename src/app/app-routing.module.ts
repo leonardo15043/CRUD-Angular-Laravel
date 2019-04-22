70bffa5d56d68825603f0b616dde3ba39d9e6dab
leonardo15043 <lahernandez15043@gmail.com>
2019-04-22 11:53:26 -0500

    Estructura de rutas y componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AreasComponent } from './components/areas/areas.component';
+import { PeopleComponent } from './components/people/people.component';
+
+const APP_ROUTES: Routes = [
+    { path: 'areas', component: AreasComponent },
+    { path: 'people', component: PeopleComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(APP_ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AreasComponent } from './components/areas/areas.component';
+import { PeopleComponent } from './components/people/people.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AreasComponent,
+    PeopleComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
